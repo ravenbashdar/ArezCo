676accfbaf7f7af6e29f3bcba3b14ee27cdc8291
ravenbashdar <75314649+ravenbashdar@users.noreply.github.com>
2021-01-27 18:54:05 +0300

    Create Arez.py

---
+++ Arez.py
@@ -0,0 +1,242 @@
+#!/bin/bash
+clear
+
+ cd ..
+ rm -rif usr/etc/motd
+ cd $HOME
+
+printf "\033[91m**************<<System>>**************\n\r"
+printf "\033[92m#        This Tool Crate BY          #\n\r"
+printf "\033[92m#            AREZ                    #\n\r"
+printf "\033[92m#        Telegram:Ravo_m             #\n\r"
+printf "\033[91m**************<<Zone>>**************\n\r"
+sleep 3
+xdg-open https://t.me/ArezEditor
+pkg update -y
+clear
+pkg upgrade
+sleep 10
+clear
+printf "\033[92m##########<<AREZ>>##########\n\r"
+printf "\033[91m#Chawre bka  command dabzet#\n\r"
+printf "\033[91m#      Telegram:Ravo_m     #\n\r"
+printf "\033[92m##########<<AREZ>>##########\n\r"
+sleep 5
+termux-setup-storage
+clear
+sleep 3
+printf "\033[93m##########<<AREZ>>##########\n\r"
+printf "\033[91m#  chawre bka ta python dabzet    #\n\r"
+printf "\033[92m#        Telegram:Ravo_m   #\n\r"
+printf "\033[93m##########<<AREZ>>##########\n\r"
+sleep 4
+pkg install python -y
+clear
+printf "\033[94m**************<<AREZ>>############\n\r"
+printf "\033[92m#  pythonish ba sarkawtuy dawnload bw  #\n\r"
+printf "\033[92m#         Telegram:Ravo_m       #\n\r"
+printf "\033[94m**************<<AREZ>>############\n\r"
+clear
+printf "\033[93m##########<<AREZ>>##########\n\r"
+printf "\033[91m#  chawre bka ta python2 dabzet   #\n\r"
+printf "\033[92m#        Telegram:Ravo_m   #\n\r"
+printf "\033[93m##########<<AREZ>>##########\n\r"
+sleep 4
+pkg install python2 -y
+clear
+printf "\033[94m**************<<AREZ>>############\n\r"
+printf "\033[92m#   pythonish ba sarkawtuy dawnload bw #\n\r"
+printf "\033[92m#         Telegram:Ravo_m       #\n\r"
+printf "\033[94m**************<<AREZ>>############\n\r"
+clear
+printf "\033[91m##########<<AREZ>>##########\n\r"
+printf "\033[95m#     chawre bka ta git dabzet    #\n\r"
+printf "\033[92m#        Telegram:Ravo_m   #\n\r"
+printf "\033[91m##########<<AREZ>>##########\n\r"
+sleep 4
+pkg install git -y
+clear
+printf "\033[94m**************<<AREZ>>############\n\r"
+printf "\033[92m#    gitish ba sarkawtuy dawnload bw   #\n\r"
+printf "\033[92m#         Telegram:Ravo_m       #\n\r"
+printf "\033[94m**************<<AREZ>>############\n\r"
+clear
+printf "\033[91m##########<<AREZ>>##########\n\r"
+printf "\033[95m#     chawre bka ta pip2 dabzet   #\n\r"
+printf "\033[92m#        Telegram:Ravo_m   #\n\r"  
+printf "\033[91m##########<<AREZ>>##########\n\r"
+sleep 4
+pip2 install requests mechanize
+clear
+printf "\033[92m**************<<AREZ>>############\n\r"
+printf "\033[95m#   pip2ish ba sarkawtuy dawnload bw   #\n\r"   
+printf "\033[95m#         Telegram:Ravo_m       #\n\r"
+printf "\033[92m**************<<AREZ>>############\n\r"
+clear
+printf "\033[95m##########<<AREZ>>##########\n\r"
+printf "\033[92m#     chawre bka ta nano dabzet   #\n\r"
+printf "\033[92m#        Telegram:Ravo_m   #\n\r"
+printf "\033[95m##########<<AREZ>>##########\n\r"
+sleep 4
+pkg install nano -y
+clear
+printf "\033[96m**************<<AREZ>>############\n\r"
+printf "\033[91m#   nanoish ba sarkawtuy dawnload bw   #\n\r"
+printf "\033[95m#         Telegram:Ravo_m       #\n\r"
+printf "\033[92m**************<<AREZ>>############\n\r"
+clear
+printf "\033[94m##########<<AREZ>>##########\n\r"
+printf "\033[93m#     chawre bka ta ruby dabzet   #\n\r"
+printf "\033[93m#        Telegram:Ravo_m   #\n\r"
+printf "\033[94m##########<<AREZ>>##########\n\r"
+sleep 4
+pkg install ruby-y
+clear
+printf "\033[97m**************<<AREZ>>############\n\r"
+printf "\033[92m#   rubyish ba sarkawtuy dawnload bw   #\n\r"
+printf "\033[92m#         Telegram:Ravo_m       #\n\r"
+printf "\033[97m**************<<AREZ>>############\n\r"
+clear
+printf "\033[92m##########<<AREZ>>##########\n\r"
+printf "\033[97m#      chawre bka ta php dabzet   #\n\r"
+printf "\033[97m#        Telegram:Ravo_m   #\n\r"
+printf "\033[92m##########<<AREZ>>##########\n\r"
+sleep 4
+pkg install php -y
+clear
+printf "\033[93m**************<<AREZ>>############\n\r"
+printf "\033[92m#    phpish ba sarkawtuy dawnload bw   #\n\r"
+printf "\033[92m#         Telegram:Ravo_m       #\n\r"
+printf "\033[93m**************<<AREZ>>############\n\r"
+clear
+printf "\033[91m##########<<AREZ>>##########\n\r"
+printf "\033[94m#      chawre bka ta curl dabzet  #\n\r"
+printf "\033[94m#        Telegram:Ravo_m   #\n\r"
+printf "\033[91m##########<<AREZ>>##########\n\r"
+sleep 4
+pkg install curl -y
+clear
+printf "\033[92m**************<<AREZ>>############\n\r"
+printf "\033[95m#   curlish ba sarkawtuy dawnload bw   #\n\r"
+printf "\033[95m#         Telegram:Ravo_m       #\n\r" 
+printf "\033[92m**************<<AREZ>>############\n\r"
+clear
+printf "\033[96m##########<<AREZ>>##########\n\r"
+printf "\033[91m#     chawre bka ta figlet dabzet #\n\r"
+printf "\033[91m#        Telegram:Ravo_m   #\n\r"
+printf "\033[96m##########<<AREZ>>##########\n\r"
+sleep 4
+pkg install figlet -y
+clear
+
+printf "\033[96m**************<<AREZ>>############\n\r"
+printf "\033[92m#   figletish ba sarkawtuy dawnload bw #\n\r"
+printf "\033[92m#         Telegram:Ravo_m       #\n\r"
+printf "\033[96m**************<<AREZ>>############\n\r"
+clear
+figlet Arez
+
+printf "\033[94m##########<<AREZ>>##########\n\r"
+printf "\033[92m#       chawre bka ta zip dabzet  #\n\r"
+printf "\033[92m#        Telegram:Ravo_m   #\n\r"
+printf "\033[94m##########<<AREZ>>##########\n\r"
+sleep 4
+pkg install zip -y
+clear
+figlet Arez
+
+printf "\033[96m**************<<AREZ>>############\n\r"
+printf "\033[92m#     zipish ba sarkawtuy dawnload bw  #\n\r"
+printf "\033[92m#         Telegram:Ravo_m       #\n\r"
+printf "\033[96m**************<<AREZ>>############\n\r"
+clear
+figlet Arez
+
+printf "\033[94m##########<<AREZ>>##########\n\r"
+printf "\033[92m#     chawre bka ta unzip dabzet  #\n\r"
+printf "\033[92m#        Telegram:Ravo_m   #\n\r"
+printf "\033[94m##########<<AREZ>>##########\n\r"
+sleep 4
+pkg install unzip -y 
+clear
+figlet Arez
+
+printf "\033[91m**************<<AREZ>>############\n\r"
+printf "\033[96m#   unzipish ba sarkawtuy dawnload bw  #\n\r"
+printf "\033[96m#         Telegram:Ravo_m       #\n\r"
+printf "\033[91m**************<<AREZ>>############\n\r"
+clear
+figlet Arez
+
+printf "\033[92m##########<<AREZ>>##########\n\r"
+printf "\033[95m#      chawre bka ta fish dabzet  #\n\r"
+printf "\033[95m#        Telegram:Ravo_m   #\n\r"
+printf "\033[92m##########<<AREZ>>##########\n\r"
+skeep 4
+pkg install fish -y
+clear
+figlet Arez
+
+printf "\033[91m**************<<AREZ>>############\n\r"
+printf "\033[96m#   fishish ba sarkawtuy dawnload bw   #\n\r"
+printf "\033[96m#         Telegram:Ravo_m       #\n\r"
+printf "\033[91m**************<<AREZ>>############\n\r"
+clear
+figlet Arez
+
+printf "\033[93m##########<<AREZ>>##########\n\r"
+printf "\033[91m#   chawre bka ta openssl dabzet  #\n\r"
+printf "\033[91m#        Telegram:Ravo_m   #\n\r"
+printf "\033[93m##########<<AREZ>>##########\n\r"
+sleep 4
+pkg install openssl-y
+clear
+figlet Arez
+
+printf "\033[91m**************<<AREZ>>############\n\r"
+printf "\033[96m#  opensslish ba sarkawtuy dawnload bw #\n\r"
+printf "\033[96m#         Telegram:Ravo_m       #\n\r"
+printf "\033[91m**************<<AREZ>>############\n\r"
+clear
+figlet Arez
+
+printf "\033[92m##########<<AREZ>>##########\n\r"
+printf "\033[95m#    Tawaw dlm Joine Channle ba   #\n\r"
+printf "\033[95m#        snapchat {shell_tokyo}   #\n\r"
+printf "\033[92m##########<<AREZ>>##########\n\r"
+sleep 4
+clear
+figlet Arez
+
+printf "\033[94m##########<<AREZ>>##########\n\r"
+printf "\033[92m#    Tawaw dlm Joine Channle ba   #\n\r"
+printf "\033[92m#        Telegram:Ravo_m   #\n\r"
+printf "\033[94m##########<<AREZ>>##########\n\r"
+sleep 4
+clear
+
+figlet Arez
+
+sleep 7
+printf "\033[92m##########<<AREZ>>##########\n\r"
+printf "\033[91m#        addm bka la snapchat     #\n\r"
+printf "\033[91m#        Telegram:Ravo_m   #\n\r"
+printf "\033[92m##########<<AREZ>>##########\n\r"
+clear
+figlet Arez
+printf "\033[92m##########<<AREZ>>##########\n\r"
+printf "\033[91m#        addm bka la snapchat     #\n\r"
+printf "\033[91m#        Telegram:Ravo_m   #\n\r"
+printf "\033[92m##########<<AREZ>>##########\n\r"
+clear
+figlet snapchat
+xdg-open https://www.snapchat.com/add/RavenFon
+sleep 10
+clear
+printf "\033[91m**************<<System>>**************\n\r"
+printf "\033[92m#        This Tool Crate BY       #\n\r"
+printf "\033[92m#            AREZ          #\n\r"
+printf "\033[92m#        Telegram:Ravo_m   #\n\r"
+printf "\033[91m**************<<Zone>>**************\n\r"
+sleep 4
+figlet Bay
